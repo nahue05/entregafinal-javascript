2950f8ef9aab0777ef27cd8cb34695619d7b443e
nahuel <christiannahuelberrutti25@gmail.com>
2022-08-15 20:38:24 -0300

    entrega final

---
+++ catalogo.js
@@ -0,0 +1,15 @@
+const lista = document.getElementById("lista");
+fetch("./catalogo.json")
+.then(resp => resp.json())
+.then(productos => {
+    productos.forEach(producto => {
+        const li = document.createElement("li");
+        li.innerHTML = `
+        <h1>${producto.Marca}</h1>
+        <img src="${producto.Imagen}">
+        <h3>Precio por dia:${producto.Precio}</h3>
+        <hr/>
+        `;
+        lista.append(li)
+    })
+})

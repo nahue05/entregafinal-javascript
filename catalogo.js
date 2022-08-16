20a84937c4e8b5dc03aea10c52b6d07a7bbf1a68
nahuel <christiannahuelberrutti25@gmail.com>
2022-08-16 20:05:36 -0300

    entrega desafio final

--- catalogo.js
+++ catalogo.js
@@ -1,15 +1,24 @@
-const lista = document.getElementById("lista");
+const carta = document.getElementById("carta");
 fetch("./catalogo.json")
 .then(resp => resp.json())
 .then(productos => {
     productos.forEach(producto => {
-        const li = document.createElement("li");
-        li.innerHTML = `
-        <h1>${producto.Marca}</h1>
-        <img src="${producto.Imagen}">
-        <h3>Precio por dia:${producto.Precio}</h3>
-        <hr/>
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <div class="container">
+            <div class="card">
+                <figure>
+                <img src="${producto.Imagen}">
+                </figure>
+                <div class="contenido">
+                <h3>${producto.Marca}</h3>
+                <p>Precio por dia: ${producto.Precio}</p>
+                <a href="./registro.html">Alquilar</a>
+                </div>
+            </div>
+        </div>
         `;
-        lista.append(li)
+        carta.append(div)
     })
 })
+
